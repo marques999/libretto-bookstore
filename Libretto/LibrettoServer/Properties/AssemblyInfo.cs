bc5c2fe7d2c1008bf637c9d6b81effafde0b2263
Diogo Marques <xmarques999@hotmail.com>
2017-04-10 17:33:33 +0100

    Cleaned assemblies

--- Libretto/LibrettoServer/Properties/AssemblyInfo.cs
+++ Libretto/LibrettoServer/Properties/AssemblyInfo.cs
@@ -1,39 +1,10 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Libretto Server")]
-[assembly: AssemblyDescription("Libretto Server")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("LibrettoServer")]
 [assembly: AssemblyCompany("FEUP")]
-[assembly: AssemblyProduct("Libretto")]
-[assembly: AssemblyCopyright("Copyright © 2017 Diogo Marques")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyProduct("LibrettoServer")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("589a4746-aa39-4e0e-8aaa-433724869a66")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
-
