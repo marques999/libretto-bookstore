e1b95142c12c741f4046d704f5802ded9e6e9dc0
Diogo Marques <xmarques999@hotmail.com>
2017-05-15 00:43:41 +0100

    Organized binaries

---
+++ Assembly/Client/LibrettoInvoiceTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Libretto.Messaging;
+using Libretto.Model;
+
+namespace Libretto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class InvoiceTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                MessagingCommon.InitializeInvoiceQueue().Send(new Invoice
+                {
+                    Quantity = 5,
+                    Total = 9.99,
+                    Title = "Dummy Book A",
+                    Timestamp = DateTime.Now,
+                    Customer = "Diogo Marques",
+                    Identifier = Guid.NewGuid()
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
